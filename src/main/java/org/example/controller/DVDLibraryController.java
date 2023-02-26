3e445f10128b26404b4c02106631db0c4496e097
vanessa-ig <66692723+vanessa-ig@users.noreply.github.com>
2023-02-26 01:18:29 +0000

    View shows simple main menu and controller gets user selection

--- src/main/java/org/example/controller/DVDLibraryController.java
+++ src/main/java/org/example/controller/DVDLibraryController.java
@@ -4,7 +4,52 @@ package org.example.controller;
 // it knows what needs to and can be done by each component
 // uses DVDLibrary interface to reference the DAO -- unaware of implementation details
 
+import org.example.ui.DVDLibraryView;
+
+import java.util.Scanner;
+
 public class DVDLibraryController {
 
-    // define two members -- one is DVDLibrary, the other is DVDLibraryView
-}
+    // define two members -- one is DVDLibrary, the other is DVDLibraryView??????
+    // This is a use of composition -- DVDLibraryController has-a DVDLibraryView
+    private DVDLibraryView view = new DVDLibraryView();
+
+    // private method to get the final menu selection
+    private int getUserSelection() {
+        return view.getMenuSelection();
+    }
+
+    public void run() {
+
+        boolean running = true;
+        int selection = 0;
+        while (running) {
+
+            selection = getUserSelection();
+
+            switch (selection) {
+                case 1:
+                    System.out.println("Add DVD");
+                    break;
+                case 2:
+                    System.out.println("Remove DVD");
+                    break;
+                case 3:
+                    System.out.println("Edit existing DVD");
+                    break;
+                case 4:
+                    System.out.println("List all DVDs");
+                    break;
+                case 5:
+                    System.out.println("Search for a DVD");
+                    break;
+                case 6:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("\nPlease pick a value from 1-6.");
+
+            } // End of switch-case
+        } // End of while loop
+    } // End of run function
+} // End of class definition
--- src/main/java/org/example/ui/DVDLibraryView.java
+++ src/main/java/org/example/ui/DVDLibraryView.java
@@ -4,17 +4,24 @@ package org.example.ui;
 // Uses UserIO interface to interact with the user -- completely aware of implementation ddetails
 
 
+import java.util.Scanner;
+
 // define a member that is of type UserIO
 public class DVDLibraryView {
 
-    public static void showMainMenu() {
-        System.out.println("==== DVD LIBRARY MAIN MENU ====");
+    public int getMenuSelection() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n==== DVD LIBRARY MAIN MENU ====");
         System.out.println("1. Add DVD to collection");
         System.out.println("2. Remove DVD from collection");
         System.out.println("3. Edit an existing DVD");
         System.out.println("4. List all DVDs in collection");
         System.out.println("5. Search for DVD by title");
-        System.out.println("6. Exit");
-    }
+        System.out.println("6. Exit\n");
 
-}
+        return input.nextInt();
+
+    } // End of getMenuSelection function
+
+} // End of class definition
