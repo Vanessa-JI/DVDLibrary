98fe4a77f3f28f19d05fa757452639c3aa1d274b
vanessa-ig <66692723+vanessa-ig@users.noreply.github.com>
2023-02-26 23:56:35 +0000

    Finished commenting on App class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,14 +4,11 @@ import org.example.controller.DVDLibraryController;
 import org.example.dao.DVDLibraryDao;
 import org.example.dao.DVDLibraryDaoException;
 import org.example.dao.DVDLibraryDaoFileImpl;
-import org.example.dto.DVD;
 import org.example.ui.DVDLibraryView;
-
-import java.io.*;
-import java.util.*;
 
 public class App {
 
+    // driving force of the application -- instantiates necessary layers (View, DAO, and Controller) and starts execution
     public static void main(String[] args) throws DVDLibraryDaoException {
         DVDLibraryView myView = new DVDLibraryView();
         DVDLibraryDao myDao = new DVDLibraryDaoFileImpl();
